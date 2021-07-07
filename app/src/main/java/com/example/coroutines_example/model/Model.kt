12971b2dbcd6052c142e955e25b8147002864ce3
thonghkk <nguyenvanthonghkk@gmail.com>
2021-07-07 21:32:31 +0700

    x

--- app/src/main/java/com/example/coroutines_example/model/Model.kt
+++ app/src/main/java/com/example/coroutines_example/model/Model.kt
@@ -3,3 +3,4 @@ package com.example.coroutines_example.model
 import java.io.Serializable
 
 class Model(val title: String? = "", val description: String? = ""):Serializable
+class Model1(val title: String? = "", val description: String? = ""):Serializable
